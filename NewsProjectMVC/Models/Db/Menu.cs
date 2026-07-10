3469944e2882d4c8a6685b79df63f5706f80af42
MBANS8A1 <seanmbng@aol.com>
2026-07-10 16:02:59 +0100

    I installed the NuGet packages (EntityFrameworkCore Tool, Design and SqlServer along with packages for CodeGenerators, Protocol and Packaging) as I want to use the database first approach on top of ADO.NET to generate the DbContext and models, which will work with the database tables, View(s) and Controller(s). I scaffolded the Menu table I made with SqlServer SSMS by running the command needed in the Package Manager Console.

---
+++ NewsProjectMVC/Models/Db/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewsProjectMVC.Models.Db;
+
+public partial class Menu
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Link { get; set; } = null!;
+
+    public int? ParentId { get; set; }
+
+    public short? Priority { get; set; }
+}
