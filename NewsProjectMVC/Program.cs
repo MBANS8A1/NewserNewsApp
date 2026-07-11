423acbd62a02fd7119f30df9d54f29cdf9b9ff6e
MBANS8A1 <seanmbng@aol.com>
2026-07-11 13:35:18 +0100

    I created a new area for my NewsProjectMVC called Admin, which I activated by adding a route to this area's Home controller with the MapControllerRoute() in the Program.cs file. The pattern is similar to other controller routes, but with a check that the area is in existence before a request is forwarded.

--- NewsProjectMVC/Program.cs
+++ NewsProjectMVC/Program.cs
@@ -17,6 +17,10 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
+    name: "Admin",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+ );
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
