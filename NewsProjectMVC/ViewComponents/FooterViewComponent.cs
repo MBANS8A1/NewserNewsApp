aa801a3078f0d495555d5df3a1cdb567b66aee25
MBANS8A1 <seanmbng@aol.com>
2026-07-23 22:20:55 +0100

    Within the InvokeAsync() method of the FooterViewComponent class, I created a variable called footer_result, which holds the instance of FooterViewModel. Next, I set the Settings property of the instance to be the first record of the dbo.Setttings table, which is fetched using the DbContext. This record is then passed to the Default.cshtml (View) within the Footer component folder when the View is returned.

--- NewsProjectMVC/ViewComponents/FooterViewComponent.cs
+++ NewsProjectMVC/ViewComponents/FooterViewComponent.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NewsProjectMVC.Models.Db;
+using NewsProjectMVC.Models.ViewModels;
 
 public class FooterViewComponent : ViewComponent
 {
@@ -15,8 +16,9 @@ public class FooterViewComponent : ViewComponent
     // GET: MENUS
     public async Task<IViewComponentResult> InvokeAsync()
     {
-       
-        return View();
+        var footer_result = new FooterViewModel();
+        footer_result.Settings = await _context.Settings.FirstOrDefaultAsync();
+        return View(footer_result);
     }
 
 }
