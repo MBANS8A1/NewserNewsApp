538716449118713914b5b5635c90340cd4160856
MBANS8A1 <seanmbng@aol.com>
2026-07-29 21:07:31 +0100

    I have added the logic for returning the View with the weather ViewModel data from the InvokeAsync() method inside the WeatherViewComponent.cs file. I created a the ViewModels/WeatherViewModel.cs file with the Temperature, City and CurrentDare properties. In the InvokeAsync() method I used the HttpClient to fetch the Open Meteo weather data for London as a JSON string (using the weather_url from the WeatherAPI static class), which I then converted into a weatherData JsonNode object as the object is the parent of all reference types. Next, I extracted the hourly temperatures and times as C# arrays and used the DateTime format to find the current temperature for the specific date and time in the data and casted it to an int for better readbility with the help of the Select LINQ method and the List FindIndex() method. Once this was all done, I passed the data to the properties of the WeatherViewModel, which is then sent to the View. If there any exceptions (e.g HttpRequestException), then a ViewModel with error data is passed to the View to provide proper feedback.

---
+++ NewsProjectMVC/Models/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NewsProjectMVC.Models.ViewModels
+{
+    public class WeatherViewModel
+    {
+        public string City { get; set; }
+        public int Temperature { get; set; }
+        public DateTime CurrentDate { get; set; }
+    }
+}
--- NewsProjectMVC/ViewComponents/WeatherViewComponent.cs
+++ NewsProjectMVC/ViewComponents/WeatherViewComponent.cs
@@ -1,7 +1,10 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NewsProjectMVC;
 using NewsProjectMVC.Models.Db;
+using NewsProjectMVC.Models.ViewModels;
+using System.Text.Json.Nodes;
 
 public class WeatherViewComponent : ViewComponent
 {
@@ -15,8 +18,50 @@ public class WeatherViewComponent : ViewComponent
     // GET: MENUS
     public async Task<IViewComponentResult> InvokeAsync()
     {
-       
-        return View();
+
+        const string apiUrl = WeatherAPI.weather_url;
+
+        using (var httpClient = new HttpClient())
+        {
+            try
+            {
+                string? jsonResponse = await httpClient.GetStringAsync(apiUrl);
+                var weatherData = JsonNode.Parse(jsonResponse);
+
+                var timeArray = weatherData["hourly"]["time"].AsArray();
+                var tempArray = weatherData["hourly"]["temperature_2m"].AsArray();
+
+               
+                var currentDateHourString = DateTime.Now.ToString("yyyy-MM-dd'T'HH:00");
+
+                var timeList = timeArray.Select(timeItem => timeItem.GetValue<string>()).ToList();
+                int currentIndex = timeList.FindIndex(time => time == currentDateHourString);
+
+                int currentTemp = 0;
+                if (currentIndex != -1)
+                {
+                    currentTemp = (int)Math.Round(tempArray[currentIndex].GetValue<double>());
+                }
+
+                var viewModel = new WeatherViewModel()
+                {
+                    Temperature = currentTemp,
+                    City = "LONDON",
+                    CurrentDate = DateTime.Now
+                };
+                 
+                return View(viewModel);
+            }
+            catch (Exception ex)
+            {
+                // If any error occurs (e.g., API is down), log the exception for debugging.
+                Console.WriteLine($"Error fetching weather data: {ex.Message}");
+
+                // Return the view with default error data.
+                var errorViewModel = new WeatherViewModel { Temperature = 0, City = "Error", CurrentDate = DateTime.Now };
+                return View(errorViewModel);
+            }
+        }
     }
 
 
