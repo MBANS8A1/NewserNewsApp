6948b6e90cb98d5c78f3a108a3a38ac2c2dd387c
MBANS8A1 <seanmbng@aol.com>
2026-08-01 14:17:13 +0100

    I generated the NewsController and the News folder Views using the Entity Framework; this will be important for routing requests for news content appropriately.

---
+++ NewsProjectMVC/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,149 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewsProjectMVC.Models.Db;
+
+public class NewsController : Controller
+{
+    private readonly MyNewsContext _context;
+
+    public NewsController(MyNewsContext context)
+    {
+        _context = context;
+    }
+
+    // GET: NEWSS
+    public async Task<IActionResult> Index()    
+    {
+        return View(await _context.News.ToListAsync());
+    }
+
+    // GET: NEWSS/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var news = await _context.News
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (news == null)
+        {
+            return NotFound();
+        }
+
+        return View(news);
+    }
+
+    // GET: NEWSS/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: NEWSS/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Title,ShortDescription,LongDescription,CreatedAt,ViewCount,Status,ImageName,CategoryId,Tags,UserId")] News news)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(news);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(news);
+    }
+
+    // GET: NEWSS/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var news = await _context.News.FindAsync(id);
+        if (news == null)
+        {
+            return NotFound();
+        }
+        return View(news);
+    }
+
+    // POST: NEWSS/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int? id, [Bind("Id,Title,ShortDescription,LongDescription,CreatedAt,ViewCount,Status,ImageName,CategoryId,Tags,UserId")] News news)
+    {
+        if (id != news.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(news);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NewsExists(news.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(news);
+    }
+
+    // GET: NEWSS/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var news = await _context.News
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (news == null)
+        {
+            return NotFound();
+        }
+
+        return View(news);
+    }
+
+    // POST: NEWSS/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int? id)
+    {
+        var news = await _context.News.FindAsync(id);
+        if (news != null)
+        {
+            _context.News.Remove(news);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool NewsExists(int? id)
+    {
+        return _context.News.Any(e => e.Id == id);
+    }
+}
