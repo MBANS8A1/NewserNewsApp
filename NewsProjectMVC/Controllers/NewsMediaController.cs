21455e8551eb9af2c0a244d9b3b3a5fae46056ed
MBANS8A1 <seanmbng@aol.com>
2026-08-13 18:49:27 +0100

    I want to create a new details page for a piece of news. So, I created a new controller in the root project Controllers folder called NewsMediaController. I added the MyNewsContext as an class member variable and the parameterized constructor to initialize the context member.

---
+++ NewsProjectMVC/Controllers/NewsMediaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsProjectMVC.Models.Db;
+
+namespace NewsProjectMVC.Controllers
+{
+    public class NewsMediaController : Controller
+    {
+        private readonly MyNewsContext _context;
+
+        public NewsMediaController(MyNewsContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
