1f58758fdfc50a29933b992d0b5b94fbea837d56
MBANS8A1 <seanmbng@aol.com>
2026-08-14 07:42:11 +0100

    To load the news articles on a new detail page, I am using the NewsMediaController in the root Controllers folder of the project.  The NewsDetails action method I added is a getter using the URL/path ("news/{id}"). The news record is requested using the single id parameter with the FirstOrDefaultAsync() method; assuming this is successful the ViewCount is incremented and the DbContext (MyNewsContext) is updated and saved with the news information. I want to add some other information to the detail view like comments, reading time, related news, popular news, popular categories. These specific features will be displayed within the detail view with the help of a NewsDetailsViewModel I will create.

--- NewsProjectMVC/Controllers/NewsMediaController.cs
+++ NewsProjectMVC/Controllers/NewsMediaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NewsProjectMVC.Models.Db;
 
 namespace NewsProjectMVC.Controllers
@@ -15,5 +16,50 @@ namespace NewsProjectMVC.Controllers
         {
             return View();
         }
+
+
+        [HttpGet("news/{id}")]
+        public async Task<IActionResult> NewsDetails(int id)
+        {
+            var news = await _context.News.FirstOrDefaultAsync(newsRecord => newsRecord.Id == id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+            news.ViewCount++;
+            _context.News.Update(news);
+            await _context.SaveChangesAsync();
+
+            //------------------
+            //I will use these later when I create more tables in the SQL Server database
+            //var comments = await _context.Comments.Where(x => x.NewsId == id && x.IsApproved).OrderByDescending(x => x.Id).ToListAsync();
+
+            //var popularCategories = await _context.PopularCategories.OrderByDescending(x => x.NewsCount).Take(10).ToListAsync();
+
+            //var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == news.CategoryId);
+
+            //var relatedNews = await _context.News.Where(x => x.CategoryId == category.Id && x.Id != news.Id).Take(2).ToListAsync();
+
+            //var currentNewsId = news.Id;
+
+            //var popularNews = await _context.PopularNews.OrderByDescending(x => x.CommentCount).Take(5).ToListAsync();
+
+            //------------------
+            var result = new NewsDetailsViewModel()
+            {
+                News = news,
+                //Comments = comments,
+                //Category = category,
+                //ReadingTimeInMinutes = TextHelpers.CalculateReadingTime(news.LongDescription),
+                //RelatedNews = relatedNews,
+                //PopularCategories = popularCategories,
+                //PopularNews = popularNews
+            };
+            //------------------
+
+            return View(result);
+        }
+
+
     }
 }
