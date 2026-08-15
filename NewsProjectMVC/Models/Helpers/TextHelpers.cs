a359408fe4b34357454e4a70e302a15ef1dfdd86
MBANS8A1 <seanmbng@aol.com>
2026-08-15 11:31:59 +0100

    This static method CalculateReadingTime in the TextHelpers static class calculated the reading based on the text passed to it by first splitting the string of text into an array (using escape sequences). If there are words in the wordCount array then it is divided by the WordsPerMinutes to return an estimated reading time in minutes. The text parameter is supplied from the news record's LongDescription found by {id} in the NewsDetails action method of the NewsMediaController.

---
+++ NewsProjectMVC/Models/Helpers/TextHelpers.cs
@@ -0,0 +1,33 @@
+﻿using NewsProjectMVC.Models.Db;
+
+namespace NewsProjectMVC.Models.Helpers
+{
+    public static class TextHelpers
+    {
+        private const int WordsPerMinute = 240; // Average reading speed (I found this from online searching)
+
+        /// <summary>
+        /// Calculates the estimated reading time in minutes for a given text.
+        /// </summary>
+        /// <param name="text">The text (LongDescription) content to analyse.</param>
+        /// <returns>The estimated reading time in minutes.</returns>
+        public static int CalculateReadingTime(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return 0;
+            }
+
+            // Count the words, splitting by spaces and newlines
+            var wordCount = text.Split([ ' ', '\r', '\n' ],
+                                    StringSplitOptions.RemoveEmptyEntries).Length;
+
+            if (wordCount == 0) return 0;
+
+            // Calculate minutes and round up to the nearest whole number
+            var minutes = Math.Ceiling((double)wordCount / WordsPerMinute);
+
+            return Convert.ToInt32(minutes);
+        }
+    }
+}
