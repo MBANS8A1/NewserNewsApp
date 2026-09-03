ffbe38e73e8ecd5263c4e783c2040a25a86c1f20
MBANS8A1 <seanmbng@aol.com>
2026-09-03 15:48:04 +0100

    In order to control what properties are displayed in the Views/Home/Index.cshtml page, I created a HomeViewModel class with properties. Inside I defined the MainNews, TopStory, FeaturedNews, BestNews and a MainPageCategories property which is a List of another ViewModel instance (MainPageCategoryViewModel. MainNews and TopStory are nullable like their fields in the Settings table even though they are of type News. The Category is nullable within the MainPageCategoryViewModel as the administrator can choose which categories to add or not. The LatestNews and MostViewsNews I will add later as these need to be derived from a View table in SSMS called NewsView, which I have yet to create.

---
+++ NewsProjectMVC/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using NewsProjectMVC.Models.Db;
+
+namespace NewsProjectMVC.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        // For single news items like MainNews and TopStory
+        public News? MainNews { get; set; }
+        public News? TopStory { get; set; }
+
+        // For lists of news items like FeaturesNews and BestNews
+        public List<News> FeaturedNews { get; set; } = new List<News>();
+        public List<News> BestNews { get; set; } = new List<News>();
+       // public List<News> LatestNews { get; set; } = new List<NewsView>();
+       // public List<News> MostViewsNews { get; set; } = new List<NewsView>();
+
+        public List<MainPageCategoryViewModel> MainPageCategories { get; set; } = new List<MainPageCategoryViewModel>();
+
+
+    }
+
+    public class MainPageCategoryViewModel
+    {
+        public Category? Category { get; set; }
+        public List<News> News { get; set; } = new List<News>();
+    }
+}
