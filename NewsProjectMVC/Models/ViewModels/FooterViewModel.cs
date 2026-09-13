701377d5d2928fdf102657f9eb514a25fd88e14f
MBANS8A1 <seanmbng@aol.com>
2026-09-13 14:20:27 +0100

    I added the necessary Categories, RecentPosts and Gallery properties to the FooterViewModel class.

--- NewsProjectMVC/Models/ViewModels/FooterViewModel.cs
+++ NewsProjectMVC/Models/ViewModels/FooterViewModel.cs
@@ -4,5 +4,10 @@ namespace NewsProjectMVC.Models.ViewModels
     public class FooterViewModel
     {
         public Setting Settings { get; set; } = new Setting();
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+        public List<News> RecentPosts { get; set; } = new List<News>();
+
+        public List<News> Gallery { get; set; } = new List<News>();
     }
 }
