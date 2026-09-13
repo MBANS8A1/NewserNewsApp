aa585f9d00433e9e6e5415046e5d8910a9025c53
MBANS8A1 <seanmbng@aol.com>
2026-09-13 12:09:22 +0100

    Within the InvokeAsync() method in the FooterViewComponent, I added a section to load the Categories, RecentPosts and Gallery images, which will end up in the Footer/Default.cshtml file for the View.  The footer_result variable already accesses the first row of the Settings model; with this I accessed the MainPageCategories field (datatype: nvarchar) and converted it to a list of integers, which is the ids of the categories in the dbo.Category table. This list is then used to return a list of categories whose ids are in the list of mainPageCategoriesIds. The two published RecentPosts in a list are created by accessing the News via the context and ordering by the CreatedAt column/field (I could have done it by the newsItem Id). The Gallery of images is created in a similar way but by ordering using the Guid.

--- NewsProjectMVC/ViewComponents/FooterViewComponent.cs
+++ NewsProjectMVC/ViewComponents/FooterViewComponent.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NewsProjectMVC.Models.Db;
 using NewsProjectMVC.Models.ViewModels;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxTokenParser;
 
 public class FooterViewComponent : ViewComponent
 {
@@ -18,6 +19,32 @@ public class FooterViewComponent : ViewComponent
     {
         var footer_result = new FooterViewModel();
         footer_result.Settings = await _context.Settings.FirstAsync();
+
+        // -----
+        //----- Section to load data for the "Recent Posts" column of the footer area -----
+
+        // 1. Get the list of selected category IDs from the settings string.
+        var mainPageCategoriesIds = footer_result.Settings.MainPageCategories?.Split(',').Select(int.Parse).ToList();
+
+        // 2. Fetch all the required Category objects in a single query.
+        footer_result.Categories = await _context.Categories
+                                         .Where(category => mainPageCategoriesIds.Contains(category.Id))
+                                         .ToListAsync();
+
+        // 3. Retrieve the published News posts ordered by the CreatedAt field/column and limit the number returned
+        footer_result.RecentPosts = await _context.News
+            .Where(newsItem => newsItem.Status == "Publish")
+            .OrderByDescending(newsItem => newsItem.CreatedAt)
+            .Take(2)
+            .ToListAsync();
+
+        // 4. Retrieve the images for the Gallery column from the published News posts by the Guid and limit the number returned
+        footer_result.Gallery = await _context.News
+                           .Where(newsItem => newsItem.Status == "Publish")
+                           .OrderBy(newsItem => Guid.NewGuid())
+                           .Take(6)
+                           .ToListAsync();
+        // -----
         return View(footer_result);
     }
 
