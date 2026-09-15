b7690e440bc4714f58eca921f7266f212d32c5c1
MBANS8A1 <seanmbng@aol.com>
2026-09-15 14:19:49 +0100

    In the root of the project, I created a new controller called NewsletterController; this will deal with the request from the input element adjacent to the "Subscribe Now" button on the Newsers home page. The email want typed and submitted by the site visitor will allow them to join something (become a subscriber to) like a newsletter mailing list for News they desire.

---
+++ NewsProjectMVC/Controllers/NewsletterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewsProjectMVC.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
