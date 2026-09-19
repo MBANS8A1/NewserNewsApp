f9cc357a98ae5c547543ea931387a0e778e85926
MBANS8A1 <seanmbng@aol.com>
2026-09-19 09:28:20 +0100

    I added the properties for the LoginViewModel class; I annotated the Username with the Required ErrorMessage and the Display Name; the same was done for the Password but with the DataType being Password.

--- NewsProjectMVC/Models/ViewModels/LoginViewModel.cs
+++ NewsProjectMVC/Models/ViewModels/LoginViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace NewsProjectMVC.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsProjectMVC.Models.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Please enter your username")]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
     }
 }
